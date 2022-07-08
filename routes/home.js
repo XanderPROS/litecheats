f55f800ee4a96fd03eced2f1f78835f948c401ad
Danish <danishahmed448@gmail.com>
2022-07-08 13:19:13 +0530

    g

--- routes/home.js
+++ routes/home.js
@@ -1,64 +1,74 @@
 var app = require("express");
-var router=app.Router();
+var router = app.Router();
 var middleware = require("../middleware");
-const {client,urlFor} = require('../utils/client');
-const {dateString} = require('../utils/dateString');
+const { client, urlFor } = require('../utils/client');
+const { dateString } = require('../utils/dateString');
 const sendgrid = require('@sendgrid/mail');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createGzip } = require('zlib');
+const sitemap = require('sitemap');
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-
-router.get("/home",async(req,res)=>{
+var CronJob = require('cron').CronJob;
+let sitemapData;
+new CronJob('0 0 */1 * * *',async()=>{
+    await generateSitemap()
+},null,true);
+router.get("/home", async (req, res) => {
     const query = '*[_type=="post"]{_createdAt,author->,body,categories[0]->,mainImage,slug,title}';
     let posts = await client.fetch(query);
-    
-    posts =  posts.map((post)=>({...post,
-        mainImage:String(urlFor(post.mainImage)),
-        author:{...post.author,image:String(urlFor(post.author.image))},
+
+    posts = posts.map((post) => ({
+        ...post,
+        mainImage: String(urlFor(post.mainImage)),
+        author: { ...post.author, image: String(urlFor(post.author.image)) },
     }))
-    
-    res.render("main/home",{posts:posts,dateString:dateString});
+
+    res.render("main/home", { posts: posts, dateString: dateString });
 })
 
-router.get("/posts/:postCategorySlug/:postSlug",async(req,res)=>{
+router.get("/posts/:postCategorySlug/:postSlug", async (req, res) => {
     const postCategorySlug = req.params.postCategorySlug;
     const postSlug = req.params.postSlug;
-    console.log(postSlug)
+
     const query = `*[_type=="post" && slug.current=="${postSlug}"]{_createdAt,author->,body,categories[0]->,mainImage,slug,title}`;
     let posts = await client.fetch(query);
-    
-    posts =  posts.map((post)=>({...post,
-        mainImage:String(urlFor(post.mainImage)),
-        author:{...post.author,image:String(urlFor(post.author.image))},
+
+    posts = posts.map((post) => ({
+        ...post,
+        mainImage: String(urlFor(post.mainImage)),
+        author: { ...post.author, image: String(urlFor(post.author.image)) },
     }))
-    res.render('main/post',{post:posts[0],dateString:dateString});
+    res.render('main/post', { post: posts[0], dateString: dateString });
 })
 
-router.get("/search/:postCategory",async(req,res)=>{
+router.get("/search/:postCategory", async (req, res) => {
     const postCategory = req.params.postCategory;
-    const query = `*[_type=="post" && categories[0]->title=="${postCategory}"]{_createdAt,author->,body,categories[0]->,mainImage,slug,title}`;
+    const query = `*[_type=="post" && (categories[0]->title=="${postCategory}" || categories[0]->slug.current=="${postCategory}")]{_createdAt,author->,body,categories[0]->,mainImage,slug,title}`;
     let posts = await client.fetch(query);
-    
-    posts =  posts.map((post)=>({...post,
-        mainImage:String(urlFor(post.mainImage)),
-        author:{...post.author,image:String(urlFor(post.author.image))},
+
+    posts = posts.map((post) => ({
+        ...post,
+        mainImage: String(urlFor(post.mainImage)),
+        author: { ...post.author, image: String(urlFor(post.author.image)) },
     }))
-    console.log(posts)
-    res.render("main/home",{posts:posts,dateString:dateString});
+
+    res.render("main/home", { posts: posts, dateString: dateString });
 })
 
-router.get("/disclaimer",async(req,res)=>{
+router.get("/disclaimer", async (req, res) => {
     res.render('main/disclaimer')
 })
-router.get("/privacy-policy",async(req,res)=>{
+router.get("/privacy-policy", async (req, res) => {
     res.render('main/privacy-policy')
 })
-router.get("/contact-us",async(req,res)=>{
+router.get("/contact-us", async (req, res) => {
     res.render('main/contact-us')
 })
-router.get("/about-us",async(req,res)=>{
+router.get("/about-us", async (req, res) => {
     res.render('main/about-us')
 })
-router.post('/send-message',async(req,res)=>{
-    try{
+router.post('/send-message', async (req, res) => {
+    try {
         const email = {
             to: 'litecheatsofficial@gmail.com',
             from: 'contact@litecheats.org',
@@ -93,28 +103,63 @@ router.post('/send-message',async(req,res)=>{
       </html>`
         }
         await sendgrid.send(email);
-        req.flash("success","Message sent successfully");
+        req.flash("success", "Message sent successfully");
         return res.redirect('/contact-us')
-    }catch(error){
+    } catch (error) {
         console.log(error)
-        req.flash("error","Server error.Try again later.");
+        req.flash("error", "Server error.Try again later.");
         return res.redirect('back')
     }
 })
-router.get('/searchPost',async(req,res)=>{
+router.get('/searchPost', async (req, res) => {
     var q = req.query.q;
-    console.log(q)
-    const regex= new RegExp(escapeRegex(q),"gi");
-    console.log(regex)
+
+    const regex = new RegExp(escapeRegex(q), "gi");
+
     const query = `*[_type=="post" && title match "${q}*"]{title,slug,categories[0]->}`;
-    console.log(query)
+
     let posts = await client.fetch(query);
-    console.log(posts)
+    posts.splice(5);
     res.json(posts)
 })
-
+router.get('/sitemap.xml', async (req, res) => {
+    try {
+        if(!sitemapData){
+            await generateSitemap();
+        }
+        res.header('Content-Type', 'application/xml');
+        res.send(sitemapData);
+    } catch (error) {
+        return res.status(500).end();
+    }
+})
+router.get('/generateSitemap',async(req,res)=>{
+    await generateSitemap();
+    res.status(200).json({'Message':'Success'})
+})
 module.exports = router;
 
 function escapeRegex(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
+
+const generateSitemap = async () => {
+    const query = '*[_type=="post"]{_createdAt,author->,body,categories[0]->,mainImage,slug,title}';
+    let catQuery = `*[_type=="category"]{slug}`;
+    let posts = await client.fetch(query);
+    let categories = await client.fetch(catQuery)
+    posts = posts.map(({ slug, categories }) => `/posts/${categories.slug.current}/${slug.current}`);
+    categories = categories.map(({ slug }) => `/search/${slug.current}`);
+    siteURLs = [...posts, ...categories];
+    console.log(siteURLs)
+    sitemapData = sitemap.createSitemap({
+        hostname: 'https://www.litecheats.org',
+        urls:siteURLs,
+        cacheTime: 600000,        // 600 sec - cache purge period 
+    });
+    
+    sitemapData.add({ url: '/home', changefreq: 'daily', priority: 0.7 })
+    sitemapData.add({ url: '/about-us', changefreq: 'monthly', priority: 0.4 })
+    sitemapData.add({ url: '/contact-us', changefreq: 'monthly', priority: 0.4 })
+    sitemapData=await sitemapData.toXML();
+}
